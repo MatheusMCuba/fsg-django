df7cc4d9fea1eea87e0f99d285f2f9d31706b9e3
MatheusCuba <matheusmcuba@gmail.com>
2018-05-17 03:05:10 +0300

    Nova View

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def apresentacao(request):
+    return HttpResponse("Olá e Boa Noite!")
